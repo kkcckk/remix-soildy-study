f9a61b2507c45f83f8aeb12d9936911e8f5cf246
kkcckk <kcxqq520@gmail.com>
2024-07-12 19:06:24 +0800

    学习时间锁

---
+++ contracts/TimeLocker.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+/**
+    时间锁：通过时间管控，对代币进行锁定，等时间到了可以安全的交易
+    也可以提前取消交易，但是只有管理员或者时间合约才能进行操作，时间合约默认是管理员的身份
+*/
+
+
+contract TimeLocker {
+    
+}
